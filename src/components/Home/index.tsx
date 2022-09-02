b09834aa20da0bc7a5c9581edf3794470b4dfdb7
W1nt3rR <frostywinterr@gmail.com>
2022-09-02 17:10:44 +0200

    Add Search functionality and Display Cards

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { newsAPI } from "../../api/api";
 import { Button } from "../Button";
+import { Card } from "../Card";
 import { Layout } from "../Layout";
 import { SearchInput } from "../SearchInput";
 import style from "./home.module.scss";
@@ -14,7 +15,7 @@ export const Home = (props : Props) => {
     const [ news, setNews ] = useState(JSON.parse(localStorage.getItem("News") as string));
     const [ search, setSearch ] = useState("");
 
-    console.log(search);
+    // console.log(search);
 
     localStorage.setItem("News", JSON.stringify(news));
 
@@ -22,9 +23,15 @@ export const Home = (props : Props) => {
         // fetchNews();
     }, [])
 
-    const fetchNews = async () => {
+    const fetchNews = async (searchParam?: string) => {
         try {
-            const result = await newsAPI.get("top-headlines", {params: { q: "tesla"}});
+            let result;
+            if(searchParam) {
+                result = await newsAPI.get("everything", {params: { q: searchParam}});
+            } else {
+                result = await newsAPI.get("top-headlines");
+            }
+
             const data = result.data.articles;
             console.log(data);
             setNews(data);
@@ -36,7 +43,13 @@ export const Home = (props : Props) => {
     return <Layout>
         <div className={style.searchContainer}>
             <SearchInput input={search} setInput={setSearch}/>
-            <Button type="coloured" onClickFn={() => {}} text="Search"/>
+            <Button type="coloured" onClickFn={() => {fetchNews(search)}} text="Search"/>
+        </div>
+            
+        <div className={style.newsContainer}>
+            {news.map((item : Object) => {
+                return <Card info={item} />
+            })}
         </div>
     </Layout>
 }
