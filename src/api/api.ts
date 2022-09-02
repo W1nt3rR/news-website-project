81aa407d115592d6d68a299a61575a3c683f0524
W1nt3rR <frostywinterr@gmail.com>
2022-09-02 09:51:40 +0200

    Rearrange API link

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const newsAPI = axios.create({
-    baseURL: "https://newsapi.org/v2/everything?apiKey=3603f9085251471c8e7e374b86b7322b",
+    baseURL: "https://newsapi.org/v2",
+    params: {
+        apiKey: "3603f9085251471c8e7e374b86b7322b"
+    }
 });
 
 export { newsAPI };
