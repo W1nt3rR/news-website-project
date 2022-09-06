0a271071b17fcccb8377b0c013af1139fa636095
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-06 13:00:44 +0200

    Remove link to original website, Add Button for routing to Article Page

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button";
 import style from "./card.module.scss";
 
 interface Props {
@@ -8,11 +10,11 @@ export const Card = (props : Props) => {
 
     const { info } = props;
 
-    return <a href={info.url} target="_blank" rel="noreferrer">
-        <div className={style.card}>
-            <img src={info.urlToImage} alt="" />
-            <h3>{info.title}</h3>
-        </div>
-    </a>
-    
+    const navigation = useNavigate();
+
+    return <div className={style.card}>
+        <img src={info.urlToImage} alt="" />
+        <h3>{info.title}</h3>
+        <Button type="coloured" text="More Info" onClickFn={() => { navigation(`/news/${info.title}`, {state: info})}}></Button>
+    </div>
 }
