b534026f2feec5a779473983cb96eb01b915a404
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-06 13:01:29 +0200

    Grab State from Location, and display image for now

--- src/components/ArticlePage/index.tsx
+++ src/components/ArticlePage/index.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom"
 import { Layout } from "../Layout"
 import style from "./article.module.scss"
 
@@ -5,8 +6,17 @@ interface Props {
 
 }
 
-export const ArticlePage = (props: Props) => {
-    return <Layout>
+interface news {
+    [key: string]: any
+}
 
+export const ArticlePage = (props: Props) => {
+
+    const location = useLocation();
+
+    const info : news = location.state as object;
+
+    return <Layout>
+        <img src={info.urlToImage} alt="" />
     </Layout>
 }
