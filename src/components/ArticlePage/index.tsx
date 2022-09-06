e0145eb803cdc12d2ebedf47b90724167b52bfbd
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-06 12:39:39 +0200

    Initial Article Page files

---
+++ src/components/ArticlePage/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../Layout"
+import style from "./article.module.scss"
+
+interface Props {
+
+}
+
+export const ArticlePage = (props: Props) => {
+    return <Layout>
+
+    </Layout>
+}
