d6dbcbd2bf2168e82e693eb4e5050308648b6e76
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-09 11:33:27 +0200

    Truncate Title and Description to 50 characters

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,6 +9,7 @@ interface Props {
 export const Card = (props : Props) => {
 
     const { info } = props;
+    const maxLength = 50;
 
     // console.log(info.title);
 
@@ -18,9 +19,16 @@ export const Card = (props : Props) => {
         navigation(`/article/${(info.title).replace(/\s/g, '-')}`, {state: info});
     }
 
+    const truncate = (text: string) => {
+        if(text == null)
+            return;
+        return (text.length > maxLength) ? `${text.slice(0, maxLength - 1)}...` : text;
+    }
+
     return <div className={style.card}>
         <img src={info.urlToImage} alt="" />
-        <h3>{info.title}</h3>
+        <h3>{truncate(info.title)}</h3>
+        <h4>{truncate(info.description)}</h4>
         <div className="filler"></div>
         <Button type="white" text="More Info" onClickFn={openArticlePage}></Button>
     </div>
