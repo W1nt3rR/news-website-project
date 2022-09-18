1989ee2dad250b822395d2b93255bfb284054188
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-18 14:55:49 +0200

    Rework API calls

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 
-const newsAPI = axios.create({
-    baseURL: "https://newsapi.org/v2",
+const newsAmmount = 10;
+
+const newsAPItop = axios.create({
+    baseURL: "https://newsapi.org/v2/top-headlines",
     params: {
-        apiKey: "3603f9085251471c8e7e374b86b7322b"
+        apiKey: "3603f9085251471c8e7e374b86b7322b",
+        pageSize: newsAmmount
     }
 });
 
-export { newsAPI };
+const newsAPIevery = axios.create({
+    baseURL: "https://newsapi.org/v2/everything",
+    params: {
+        apiKey: "3603f9085251471c8e7e374b86b7322b",
+        pageSize: newsAmmount
+    }
+});
+
+export { newsAPItop, newsAPIevery };
