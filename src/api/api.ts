e7eaf268a8e705390907da954300d494be3b9a10
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-18 15:00:51 +0200

    Commonize params in axios calls

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,18 @@
 import axios from "axios";
 
-const newsAmmount = 10;
+const commonParams = {
+    apiKey: "3603f9085251471c8e7e374b86b7322b",
+    pageSize: 10
+}
 
 const newsAPItop = axios.create({
     baseURL: "https://newsapi.org/v2/top-headlines",
-    params: {
-        apiKey: "3603f9085251471c8e7e374b86b7322b",
-        pageSize: newsAmmount
-    }
+    params: commonParams
 });
 
 const newsAPIevery = axios.create({
     baseURL: "https://newsapi.org/v2/everything",
-    params: {
-        apiKey: "3603f9085251471c8e7e374b86b7322b",
-        pageSize: newsAmmount
-    }
+    params: commonParams
 });
 
 export { newsAPItop, newsAPIevery };
